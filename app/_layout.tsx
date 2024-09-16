354ab026f1d93c3c088eeb486483336c316ae59b
Juan Cano <juan.cano@sofka.com.co>
2024-09-16 12:14:42 -0500

    Añadir contexto de datos y proveedor en DataContext.tsx, se crean las interfaces de los datos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,21 @@
 import { AuthContextProvider } from "@/context/AuthContext";
+import { DataContextProvider } from "@/context/DataContext";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <AuthContextProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: "white" },
-        }}
-      >
-        <Stack.Screen name="Login" />
-        <Stack.Screen name="(tabs)" />
-      </Stack>
+      <DataContextProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: "white" },
+          }}
+        >
+          <Stack.Screen name="Login" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </DataContextProvider>
     </AuthContextProvider>
   );
 }
