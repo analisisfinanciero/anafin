8d469c595eb67c7db46a4cbdb75d77246ecc72fe
Juan Cano <juan.cano@sofka.com.co>
2024-10-11 13:44:01 -0500

    validacion en analisis component

--- screens/analytics/AnalyticsComponent.tsx
+++ screens/analytics/AnalyticsComponent.tsx
@@ -1,17 +1,30 @@
 import CustomAlertInformative from "@/components/CustomAlertInformative";
+import { useDataContext } from "@/context/DataContext";
 import React from "react";
 import { View } from "react-native";
 
 const AnalyticsComponent = () => {
+  const { dataInformation } = useDataContext();
+
   return (
     <View className="p-4">
-      <CustomAlertInformative
-        isVisible={true}
-        type="error"
-        message={`Ingresa los estados de resultados en "Datos".`}
-        isCloseable={false}
-        onDismiss={() => {}}
-      />
+      {dataInformation?.hasData ? (
+        <CustomAlertInformative
+          isVisible={true}
+          type="info"
+          message={`Los años NO guardados, también serán tomados en cuenta para el análisis.`}
+          isCloseable={false}
+          onDismiss={() => {}}
+        />
+      ) : (
+        <CustomAlertInformative
+          isVisible={true}
+          type="error"
+          message={`Ingresa los estados de resultados en "Datos".`}
+          isCloseable={false}
+          onDismiss={() => {}}
+        />
+      )}
     </View>
   );
 };
