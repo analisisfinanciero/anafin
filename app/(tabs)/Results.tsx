21c1b382a8019a0127623ca9f4bc80384c1effd9
Juan Cano <juan.cano@sofka.com.co>
2024-10-18 11:40:17 -0500

    se crea tab de resultados

---
+++ app/(tabs)/Results.tsx
@@ -0,0 +1,13 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import React from "react";
+import { Text } from "react-native";
+
+const Results = () => {
+  return (
+    <ScreenWrapper>
+      <Text>Interpretaciones</Text>
+    </ScreenWrapper>
+  );
+};
+
+export default Results;
