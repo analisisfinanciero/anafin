0ec8e024058299906823a8c809b1862ace38f1f0
Juan Cano <juan.cano@sofka.com.co>
2024-11-06 11:49:14 -0500

    formulario para mostrar, guardar y eliminar Resultados. Logica de resultados falta ia

--- screens/results/ResultsComponents.tsx
+++ screens/results/ResultsComponents.tsx
@@ -1,13 +1,112 @@
 import CustomAlertInformative from "@/components/CustomAlertInformative";
+import CustomFormButton from "@/components/CustomFormButton";
 import { useDataContext } from "@/context/DataContext";
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, Text, View } from "react-native";
+import ResultsService from "../../service/results/ResultsService";
+import { useAuth } from "@/context/AuthContext";
+import Spinner from "@/components/Spinner";
+import Empty from "@/components/Empty";
+import ResultsForm from "./ResultsForm";
+import uuid from "react-native-uuid";
 
 const ResultsComponents = () => {
   const { analyticsInformation } = useDataContext();
+  const { user } = useAuth();
+  const { enterpriseInformation } = useDataContext();
+
+  const [showSessionAlert, setShowSessionAlert] = useState(false);
+  const [isAvailable, setIsAvailable] = useState(false);
+  const [resultGenerated, setResultGenerated] = useState<any>(null);
+  const [resultsList, setResultsList] = useState<any[]>([]);
+
+  const [loading, setLoading] = useState({
+    result: false,
+    resultsList: false,
+  });
+
+  let resultId = uuid.v4();
+
+  useEffect(() => {
+    if (
+      analyticsInformation?.hasData &&
+      analyticsInformation?.horizontalAnalytics &&
+      analyticsInformation?.verticalAnalytics
+    ) {
+      setIsAvailable(true);
+    }
+    if (user?.id !== "usuarioIncognito") {
+      getIAResults(user?.email ?? "");
+    } else {
+      setShowSessionAlert(true);
+    }
+  }, []);
+
+  const getIAResults = async (email: string) => {
+    setLoading({ ...loading, resultsList: true });
+    ResultsService.getAllEarningsByUid(email)
+      .then((response) => {
+        const responseData: any[] = [];
+        response.forEach((doc) => {
+          responseData.push(doc.data());
+        });
+        setResultsList(responseData);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading({ ...loading, resultsList: false });
+      });
+  };
+
+  const generateIAResults = async () => {
+    const result = {
+      id: resultId,
+      title: `Resultados de la empresa ${enterpriseInformation?.enterpriseName} NIT: ${enterpriseInformation?.enterpriseNIT}`,
+      results: "Resultados de IA",
+      email: user?.email,
+    };
+    setResultGenerated(result);
+  };
+
+  const handleSaveResult = () => {
+    ResultsService.insertResults(resultGenerated)
+      .then((response) => {
+        console.log(response);
+        if (response?.id) {
+          getIAResults(user?.email ?? "");
+          Alert.alert(
+            `Guardado exitoso`,
+            `El resultado ha sido guardado`,
+            [
+              {
+                text: "Aceptar",
+                onPress: () => {},
+                style: "destructive",
+              },
+            ],
+            { cancelable: true }
+          );
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleDeleteResult = (id: string) => {
+    ResultsService.deleteResult(id)
+      .then(() => {
+        getIAResults(user?.email ?? "");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
-    <View>
+    <View className="p-4">
       {!analyticsInformation?.hasData && (
         <CustomAlertInformative
           isVisible={true}
@@ -17,6 +116,64 @@ const ResultsComponents = () => {
           onDismiss={() => {}}
         />
       )}
+      {isAvailable && (
+        <CustomAlertInformative
+          isVisible={isAvailable}
+          type="info"
+          message={`Presione el botón de "Generar resultados con IA" para obtener un análisis de los resultados obtenidos.`}
+          isCloseable={false}
+          onDismiss={() => {}}
+        />
+      )}
+      {showSessionAlert && (
+        <CustomAlertInformative
+          isVisible={showSessionAlert}
+          type="info"
+          message={`Para ver el historial de resultados, inicia sesión en "Opciones".`}
+          isCloseable={false}
+          onDismiss={() => {}}
+        />
+      )}
+
+      {isAvailable && (
+        <View className="border-b border-neutral-200 mb-2">
+          <Text className="text-[18px] font-bold mb-4 self-center">
+            Nuevo resultado
+          </Text>
+          {resultGenerated && (
+            <ResultsForm
+              key={resultGenerated.id}
+              title={resultGenerated.title}
+              resultContent={resultGenerated.results}
+              isNew={true}
+              OnPressButton={handleSaveResult}
+              disabledButton={user?.id === "usuarioIncognito"}
+            />
+          )}
+          <CustomFormButton
+            onPressFunction={generateIAResults}
+            textButton={"Generar resultados con IA"}
+          />
+        </View>
+      )}
+      <View className="mb-2 mt-10">
+        <Text className="text-[18px] font-bold mb-4 self-center">
+          Historial de resultados
+        </Text>
+        {loading.resultsList && <Spinner />}
+        {!loading.resultsList && resultsList.length === 0 && <Empty />}
+        {!loading.resultsList &&
+          resultsList.length > 0 &&
+          resultsList.map((result) => (
+            <ResultsForm
+              key={result.id}
+              isNew={false}
+              title={result.title}
+              resultContent={result.results}
+              OnPressButton={() => handleDeleteResult(result.id)}
+            />
+          ))}
+      </View>
     </View>
   );
 };
---
+++ screens/results/ResultsForm.tsx
@@ -0,0 +1,44 @@
+import CustomAccordion from "@/components/CustomAccordion";
+import CustomFormButton from "@/components/CustomFormButton";
+import { Colors } from "@/constants/Colors";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+
+interface ResultsFormProps {
+  title?: string;
+  resultContent?: string;
+  isNew?: boolean;
+  disabledButton?: boolean;
+  OnPressButton: () => void;
+}
+const ResultsForm = ({
+  title,
+  resultContent,
+  isNew,
+  disabledButton = false,
+  OnPressButton,
+}: ResultsFormProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <CustomAccordion
+      title={title ?? `Resultados sin titulo`}
+      isOpen={isOpen}
+      onToggle={toggleAccordion}
+    >
+      <View className="p-4">
+        <Text>{resultContent}</Text>
+        <CustomFormButton
+          onPressFunction={OnPressButton}
+          backgroundColor={isNew ? Colors.BlueColor : Colors.RedColor}
+          textButton={isNew ? "Guardar" : "Eliminar"}
+          isDisabled={disabledButton}
+        />
+      </View>
+    </CustomAccordion>
+  );
+};
+
+export default ResultsForm;
